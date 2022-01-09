b5834c1ffc72f590381ba2b4dfe771b39e5b21dc
Doug Irwin <me@dougirw.in>
2022-01-09 15:34:29 -0700

    Week 1 Check B

--- check01b.py
+++ check01b.py
@@ -1,3 +1,10 @@
+'''
+Program: Check01b.py
+Brother Mellor, CS 241
+Author: Doug Irwin
+Summary: Asks for your name and age. Gives you your age on your next birthday.
+'''
+
 name = input("Please enter your name: ")
 age = int(input("Please enter your age: "))
 
