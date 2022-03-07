1caaf882eca1746168b9a13bbc4842113a757283
Doug Irwin <me@dougirw.in>
2022-03-06 22:55:55 -0700

    Week 8 Assignment

--- Asteroids/flying_object.py
+++ Asteroids/flying_object.py
@@ -10,7 +10,7 @@ class FlyingObject(ABC):
     def __init__(self, img:str, radius:float, speed:float=0, angle:float=1) -> None:
         self.center:Point = Point() # Default center point is (0,0)
         self.velocity:Velocity = Velocity(speed, speed) # Speed is optional param, otherwise defaults as 1
-        self.texture = arcade.load_texture("Asteroids/"+img) # Required param
+        self.texture = arcade.load_texture(img) # Required param
         self.direction:float = 1
         self.speed:float = speed
         self.radius:float = radius # Required param
