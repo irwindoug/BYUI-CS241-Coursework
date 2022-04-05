b85f490c50338489f8a3f0de3257ea43fdcc007d
Doug Irwin <me@dougirw.in>
2022-04-04 21:18:10 -0600

    Week 13 Check B

---
+++ check13b/read_demo.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter some text > " text
+echo "You entered: $text"
+
+echo -n "Hurry up and type something! > "
+if read -t 3 response; then
+    echo "Great, you made it in time!"
+else
+    echo "Sorry, you are too slow!"
+fi
